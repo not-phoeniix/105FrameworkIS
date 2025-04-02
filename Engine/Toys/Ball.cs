ffb25a710f61807a4500c8eac3be39a7942cc767
jws8554 <joseph-staria@hotmail.com>
2025-04-02 14:27:46 -0400

    Played with my rope

---
+++ Engine/Toys/Ball.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Toys
+{
+    public class Ball : Entity
+    {
+        Color color = Color.Blue;
+        uint size = 0;
+
+        private Vector2 lastVelocity;
+
+        public Ball(Vector2 position, uint size, float mass, Color color) : base(position, new Rectangle(0,0,(int)size,(int)size),mass,1000)
+        {
+            this.size = size;
+            this.color = color;
+            Physics.GroundFrictionScale = 2;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            
+        }
+
+        public override void Draw(SpriteBatch sb)
+        {
+            sb.DrawCircleFill(Position, size, 20, color);
+        }
+    }
+}
--- Testing/MyGame.cs
+++ Testing/MyGame.cs
@@ -9,6 +9,8 @@ namespace Testing;
 public class MyGame : Engine.Game
 {
     private Rope rope;
+    private Rope rope2;
+    private Ball ball;
     private Character character;
 
     public void Init()
@@ -16,19 +18,30 @@ public class MyGame : Engine.Game
         SetResolution(500, 500);
 
         character = new Character(new Vector2(250, 250), new Rectangle(0, 0, 50, 50), 5, 5, 1000, 35);
-
+        ball = new Ball(Vector2.One * 150, 25, 1, Color.Red);
         rope = new(
             new Vector2(250, 20),
             new Vector2(400, 20),
-            20)
+            10)
+        {
+            DrawColor = Color.Black,
+            DrawThickness = 5
+        };
+        rope2 = new(
+            new Vector2(400, 20),
+            new Vector2(400, 20),
+            10)
         {
             DrawColor = Color.Black,
             DrawThickness = 5
         };
 
         character.AttachRope(rope);
+        character.AttachRope(rope2);
 
         AddToScene(character);
+        AddToScene(ball);
+        AddToScene(rope2);
         AddToScene(rope);
     }
 
